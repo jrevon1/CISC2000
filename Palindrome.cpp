fd6548aa03c68e96085fd01482557cf2ab12b6bf
jrevon1 <jrevon1@gmail.com>
2020-10-16 20:57:22 -0400

    Update Palindrome.cpp
    Removed the cleanInput string in main(). Removed the duplicate call to removePunctuation(convertToLower(inputString)) at line 72; this already happened in the isPalindrome function. Changed the return value at lines 181-185, simply returning the result of a reverseInput == cleanString comparison. Updated some of the names of the variables to account for the names from the assignment instead of my own from scratch!

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,11 +1,11 @@
  /* Palindrome.cpp
- * This program: Takes a given user input string, converts it to lowercase and removes punctuation,
- * then checks to see if that string is the exact same when reversed. If the string is the same forward
- * and backward, it is a palindrome; if not, it is not a palindrome. The result is stored accordingly in
+ * This program: Takes a given user input string, creates a copy in lowercase with no punctuation, and
+ * compares that string to a reversed copy of itself. If the string is the same forward and backward, 
+ * it is a palindrome; if not, it is not a palindrome. The result is stored accordingly in
  * a respective vector, "palindromes" and "not_palindromes", and subsequently displayed when the user
  * inputs "quit".
  * Author: Jordan Revon
- * Last modified on: 10/13/2020
+ * Last modified on: 10/16/2020
 */
 
 // Declare libraries to use
@@ -18,18 +18,18 @@ using namespace std;
 
 
 // removePunctuation creates a copy of the string and removes punctuation from the copy leaving the original alone.
-// @param inputString is the string to clean.
+// @param sentence is the string to clean.
 // @returns a cleaned string with no punctuation.
 string removePunctuation(const string sentence);
 
 
 // convertToLower returns a lowercase version of the inputString string.
-// @param inputString is the string to convert.
+// @param mixedcase is the string to convert.
 // @returns a lowercase string.
 string convertToLower(const string mixedcase);
 
 // Returns a reverse version of the cleanInput string.
-// @param cleanInput is the string to reverse.
+// @param input is the string to reverse.
 // @returns a string with the contents that's the reverse of the input.
 string reverse(string input);
 
@@ -40,40 +40,36 @@ void display(vector<string> vstrings);
 // isPalindrome passes the input string and calls all of the other 
 //  functions to prepare the string.
 // Once the string is cleaned, it tests if it's a palindrome. 
-// @param inputString is the string to check.
+// @param palindrome is the string to check.
 // @returns true if it's a palindrome and false if not.
 bool isPalindrome(string palindrome);
 
 int main()
 {
-    // Bool variable to run the following do-while loop
+    // Bool variable to continue running a do-while loop
     bool tryAgain = true;
-    // String variables for user input before, inputString, and after
-    // converting to lowercase and removing punctuation, cleanInput
-    string inputString, cleanInput;
+    // String variable for user input
+    string inputString;
     // String vectors to store palindrome inputs and inputs that are not palindromes
     vector<string> palindromes, not_palindromes;
-    // Do-while loop to gather user input, convert to lowercase and remove punctuation,
-    // and add the subsequent cleanInput to the palindrome vector or not_palindrome vector
-    // until the inputString is "quit"
+    // Do-while loop to gather input and add them to their respective lists
     do
     {
         cout << "Enter your palindrome or type quit: " << endl;
+        // Read a whole line of user input
         getline(cin, inputString);
         // Quit the loop if the inputString is "quit"
         if(inputString == "quit")
         {
             break;
         }
-        // Otherwise take the inputString and call convertToLower(), and 
-        // removePunctuation() and store the result in cleanInput
+        // Otherwise take the inputString and call isPalindrome() on it
         else
         {
-            cleanInput = removePunctuation(convertToLower(inputString));
-            // if the cleanInput is a palindrome, store it in the "palindromes" vector
-            if(isPalindrome(cleanInput))
+            // If the inputString is a palindrome, store it in the "palindromes" vector
+            if(isPalindrome(inputString))
                 palindromes.push_back(inputString);
-            // Otherwise store cleanInput in the "not_palindromes" vector
+            // Otherwise store it in the "not_palindromes" vector
             else
                 not_palindromes.push_back(inputString);
         }
@@ -90,26 +86,25 @@ int main()
 
 // removePunctuation - Removes punctuation from a given string
 // Precondition: a string that may or may not have punctuation.
-// Postcondition: returns a (possibly shorter) string with punctuaton
-// removed.
+// Postcondition: returns a (possibly shorter) string with punctuaton removed.
 string removePunctuation(const string sentence)
 {
     // Variable to make a copy of the string to manipulate
     //  without changing the input
-    string cleanInput;
+    string noPunct;
     // For-loop to remove punctuation from the given string
-    //  and store it in the copy, cleanInput
+    //  and store it in the copy, noPunct
     for(int i = 0; i < sentence.length(); i++)
     {
         // If the element at index i in the given string is a
-        //  character or number, concatenate it to cleanInput
+        //  character or number, concatenate it to noPunct
         if(isalpha(sentence[i]) || isdigit(sentence[i]))
         {
-            cleanInput += sentence[i];
+            noPunct += sentence[i];
         }
     }
-  // Return the contents of the copy, cleanInput 
-  return cleanInput;
+  // Return the contents of the copy, noPunct 
+  return noPunct;
 }
 
 // convertToLower - Converts a provided string to lowercase only
@@ -123,8 +118,8 @@ string convertToLower(const string mixedcase)
     // For-loop to change any uppercase characters to lowercase
     for(int i = 0; i < mixedcase.length(); i++)
     {
-        // Each element at index i is converted to lowercase
-        //  and concatenated to lowerInput
+        // Each element at index i is concatenated to 
+        //  lowerInput in lowercase only
         lowerInput += tolower(mixedcase[i]);
     }
     // Return the contents of the copy, lowerInput
@@ -176,11 +171,8 @@ bool isPalindrome(string palindrome)
     string cleanString = convertToLower(removePunctuation(palindrome));
     // Variable to store the cleaned string in reverse
     string reverseInput = reverse(cleanString);
-    // If the reversed string is the same as the cleaned string,
-    // it's a palindrome, so return true
-    if(reverseInput == cleanString)
-        return true;
-    // Otherwise it's not a palindrome so return false
-    else
-        return false;
+    // Return the result of a reverseInput and cleanString comparison
+    // If they're the same, return value will be "true"
+    // If they're not the same, return value will be "false"
+    return (reverseInput == cleanString);
 }
