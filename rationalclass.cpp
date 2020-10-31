c0c9143997d9e13a7d7eb0aa3b5c28a2be89f9b1
jrevon1 <jrevon1@gmail.com>
2020-10-31 18:10:07 -0400

    Update rationalclass.cpp

--- rationalclass.cpp
+++ rationalclass.cpp
@@ -1,58 +1,47 @@
-/*work in progress*/
-
 #include <iostream>
 #include <string>
 #include <stdlib.h>
 
 using namespace std;
 
-// don't forget to include variables for a TOTAL and RESULT
-// op2.input()
-// total.Add
-
-//Please follow the order suggested below when working on this lab, always maintaining a compilable version of the code. Take an incremental approach. Write and test one function at a time!
-
-
-//  Implement input function. which will read a rational number.
-//  Implement Add, Subtract, Multiply, Divide functions like but remember that we have 
-//    rational result as an accumulator for all operations. Call all functions like this 
-//    result.Add(result, op1);
 class rational
 {
 public:
-	/* DONE: default constructor set the rational number to 0, (i.e., numerator is 0, denominator is 1) */
+	/* Done: default constructor set the rational number to 0, (i.e., numerator is 0, denominator is 1) */
   rational();
+  rational(int n);
+  rational(int n, int d);
 
-	/* TODO: define input to set the invoking object's value from user input */
+	/* Done: define input to set the invoking object's value from user input */
 	void input();
 
-	/* TODO: define output to display invoking object's value in the standard output, in the form of numerator/denominator */
+	/* Done: define output to display invoking object's value in the standard output, in the form of numerator/denominator */
 	void output() const;
 
-	/* DONE: declare the accessor to return the invoking object's numerator */
+	/* Done: declare the accessor to return the invoking object's numerator */
   int get_numerator() const { return numerator;}
 	
-	/* DONE: declare the accessor to return the invoking object's denominator */
+	/* Done: declare the accessor to return the invoking object's denominator */
   int get_denominator() const { return denominator;}
 
-	/* TODO: declare Add to set the invoking object to be the sum of op1 and op2, use const & parameters */
+	/* Done: declare Add to set the invoking object to be the sum of op1 and op2, use const & parameters */
   void Add(const rational& op1, const rational& op2);
 
 
-	/* TODO: declare Subtract to set the invoking object to the difference of op1 and op2, use const & parameters */
+	/* Done: declare Subtract to set the invoking object to the difference of op1 and op2, use const & parameters */
   void Subtract(const rational& op1, const rational& op2);
 
 
-	/* TODO: declare Multiply to set the invoking object to the product of op1 and op2, use const & parameters */
+	/* Done: declare Multiply to set the invoking object to the product of op1 and op2, use const & parameters */
   void Multiply(const rational& op1, const rational& op2);
 
 
-	/* TODO: declare Divide to set the invoking object to the Quotient of op1 and op2, use const & parameters */
+	/* Done: declare Divide to set the invoking object to the Quotient of op1 and op2, use const & parameters */
   void Divide(const rational& op1, const rational& op2);
 
 
 private:
-	/* TODO: declare data members for rational object */
+	/* Done: declare data members for rational object */
   int numerator;
   int denominator;
 
@@ -62,47 +51,83 @@ private:
 int main()
 {
 	char oper;
-  rational result, op1;
-	// TODO: declare rational objects for result and operand.
+	// Done: declare rational objects for result and operand.
+  rational result, op2;
 
 	cout << "Enter op1 (in the format of p/q): ";
-	// TODO: Use your input function to read the first operand into result.
-  op1.input();
-  op1.output();
+	// Done: Use your input function to read the first operand into result.
+  result.input();
   
 
-	// //Test rational class member function 
-	// do {
-	// 	cout << "\nEnter operator [+, -, /, *, =, c(lear), a(ccessors), q(uit)]: ";
-	// 	// TODO: Read the operator into oper
-  //   // c - read in a new value to start with i.e. re-enter op1's values
-  //   // a - output numerator and output denominator
+	//Test rational class member function 
+	do {
+		cout << "\nEnter operator [+, -, /, *, =, c(lear), a(ccessors), q(uit)]: ";
+    // Done: Read the operator into oper
+    cin >> oper;
+    
+	  // Done: Implement a switch or multiway if statement with one case for each option above where
+		// '+','*','/','-' inputs a rational op1 and calculates result.oper(result,op1)
+		// '=' outputs the current result,
+		// 'c' to clear current result, use input function to read first operand into result,
+		// 'a' to test accessors, 'q' to quit.
 
-	// 	// TODO: Change the pseodocode below to test oper for binary operators
-	// 	if (oper in "+,-,*,/") {
-	// 		cout << "\nEnter op2 (in the format of p/q): ";
-	// 		// TODO: Use your input function to read the operand into operand
+    // if c - read in a new value to start with i.e. re-enter op1's values
+    if(oper == 'c')
+    {
+      cout << "\nEnter op1 (in the format of p/q): ";
+      result.input();
+    }
+		
+    // if a - output numerator and output denominator
+    else if(oper == 'a')
+    {
+      cout << "\nresult's numerator is: " << result.get_numerator() << endl;
+      cout << "result's denominator is: " << result.get_denominator();
+    }
 
-	// 	}
+		// if + add a specified op2 to result
+		else if(oper == '+')
+    {
+      cout << "\nEnter op2 (in the format of p/q): ";
+      op2.input();
+      result.Add(result, op2);
+    }
+    // if - subtract a specified op2 from result
+    else if(oper == '-')
+    {
+      cout << "\nEnter op2 (in the format of p/q): ";
+      op2.input();
+      result.Subtract(result, op2);
+    }
+    // if * multiply a specified op2 and result
+    else if(oper == '*')
+    {
+      cout << "\nEnter op2 (in the format of p/q): ";
+      op2.input();
+      result.Multiply(result, op2);
 
-	// 	// ToDo: Implement a switch or multiway if statement with one case for each option above where
-	// 	// '+','*','/','-' inputs a rational op1 and calculates result.oper(result,op1)
-	// 	// '=' outputs the current result,
-	// 	// 'c' to clear current result, use input function to read first operand into result,
-	// 	// 'a' to test accessors, 'q' to quit.
+    }
+    // if / divide a specified op2 and result
+    else if(oper == '/')
+    {
+			cout << "\nEnter op2 (in the format of p/q): ";
+      op2.input();
+      result.Divide(result, op2);
+    
+		}
+    else if(oper == '=')
+    {
+      cout << "result = "; 
+      result.output();
+    }
 
-  //   if(oper == '+')
-  //   {
-  //     result.Add(result, op2);
-  //   }
 
-
-	// } while (oper != 'q');
+	} while (oper != 'q');
 	return 0;
 }
 
 
-// TODO: Definitions of all member functions declared above
+// Done: Definitions of all member functions declared above
 
 // DONE: Default constructor sets numerator to 0 and denominator to 1
 rational::rational()
@@ -111,15 +136,35 @@ rational::rational()
   denominator = 1;
 }
 
+rational::rational(int n)
+{
+  numerator = n;
+  denominator = 1;
+}
+
+rational::rational(int n, int d)
+{
+  if(denominator <= 0)
+  {
+    cout << "Cannot set denominator to 0. Denominator set to 1" << endl;
+    denominator = 1;
+  }
+  else
+  {
+    numerator = n;
+    denominator = d;
+  }
+}
+
 void rational::input()
 {
-  char delim = '/';
-  string inputString;
-  getline(cin, inputString);
-  // before delim = numerator;
-  // after delim = denominator;
-
-
+  char slash;
+  cin >> numerator >> slash >> denominator;
+  if(denominator <= 0)
+  {
+    cout << "Cannot set denominator to 0. Denominator set to 1" << endl;
+    denominator = 1;
+  }
 
 }
 
@@ -129,41 +174,34 @@ void rational::output() const
 
 }
 
-//int rational::get_numerator() const
-//{}
-
-//int rational:: get_denominator() const
-//{}
-
-
 
 void rational::Add(const rational& op1, const rational& op2)
 {
-  // numerator = n1*d2 + n2+d1;
-  // numerator = op1.numerator*op2.denominator + op2.numerator*op1.denominator;
+  //numerator = n1*d2 + n2+d1;
+  numerator = op1.numerator*op2.denominator + op2.numerator*op1.denominator;
 
-  // denominator = d1*d2
-  // denominator = op1.denominator*op2.denominator;
+  //denominator = d1*d2
+  denominator = op1.denominator*op2.denominator;
 
 }
 
 void rational::Subtract(const rational& op1, const rational& op2)
 {
   // numerator = n1*d2 - n2+d1;
-  // numerator = op1.numerator*op2.denominator - op2.numerator*op1.denominator;
+  numerator = op1.numerator*op2.denominator - op2.numerator*op1.denominator;
 
   // denominator = d1*d2
-  // denominator = op1.denominator*op2.denominator;
+  denominator = op1.denominator*op2.denominator;
 
 }
 
 void rational::Multiply(const rational& op1, const rational& op2)
 {
   // numerator = n1*n2
-  // numerator = op1.numerator*op2.numerator;
+  numerator = op1.numerator*op2.numerator;
 
   // denominator = d1*d2
-  // denominator = op1.denominator*op2.denominator;
+  denominator = op1.denominator*op2.denominator;
   
 }
 
@@ -174,10 +212,10 @@ void rational::Divide(const rational& op1, const rational& op2)
   // (3*3)/(4*2) = 9/8
 
   // numerator = n1*d2
-  // numerator = op1.numerator*op2.denominator;
+  numerator = op1.numerator*op2.denominator;
 
   // denominator = d1*n2
-  // denominator = op1.denominator*op2.numerator;
+  denominator = op1.denominator*op2.numerator;
 
 }
 
